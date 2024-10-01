85659f5b90850b345b750e385175a4ca327a0bf6
Kingston Spotts <guitarhero0122@gmail.com>
2024-10-01 18:32:08 -0400

    mini projects

---
+++ dice-game/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+static void Main (string[] args)
+{
+int playerRandomNum;
+int enemyRandomNum;
+
+Random random = new Random();
+
+int playerPoints = 0;
+int enemyPoints = 0;
+
+for (int i = 0; i < 10; i++)
+{
+        Console.WriteLine("Press any key to roll the dice");
+
+        Console.ReadKey();
+
+        playerRandomNum = random.Next(1, 7);
+        Console.WriteLine("You rolled a " + playerRandomNum);
+
+        Console.WriteLine("... ");
+        System.Threading.Thread.Sleep(1000);
+
+
+        enemyRandomNum = random.Next(1,7);
+        Console.WriteLine("The Enemy rolled a " + enemyRandomNum);
+
+        if (playerRandomNum > enemyRandomNum)
+        {
+            playerPoints++;
+            Console.WriteLine("Player wins this round!");
+        }
+        else if (playerRandomNum < enemyRandomNum) {
+            enemyPoints++;
+            Console.WriteLine("The Enemy wins this round!");
+        }
+        else {
+            Console.WriteLine("Draw, No points awarded!");
+        }
+        Console.WriteLine("The score is now - Player : " + playerPoints);
+}
+
+if (playerPoints > enemyPoints) {
+    Console.WriteLine("You Win!");
+}
+else if (playerPoints < enemyPoints) {
+    Console.WriteLine("You Lose! Try Again?");
+}
+else {
+    Console.WriteLine("Its a Draw! Try Again?");
+}
+Console.ReadKey();
+}
---
+++ introC#/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            float num1;
+            float num2;
+            float result;
+
+            string answer;
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Please enter your first number.");
+
+        num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter your second number.");
+
+        num2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("What type of operation would you like to use? ");
+
+answer = Console.ReadLine();
+
+if (answer == "+")
+{
+    result = num1 + num2;
+}
+else if (answer == "-")
+{
+    result = num1 - num2;
+}
+else if (answer == "*")
+{
+    result = num1 *  num2;
+}
+else if (answer == "/")
+{
+    result = num1 / num2;
+}
+else
+{
+if (num2 != 0)
+{
+    result = num1 / num2;
+}
+else
+{
+    result = float.NaN;
+}
+}
+
+if (float.IsNaN(result)){
+    Console.WriteLine("Invalid operation - Cannot divide by 0!");
+}
+else {
+  Console.WriteLine("the result is " + result);
+}
+            Console.ReadKey();
+        }
+    }
+}
